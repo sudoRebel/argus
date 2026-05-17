ca91e90cba1119d49234fc0bd77da8c51deb659c
sudoRebel <mouad.scp.m@gmail.com>
2026-05-17 20:20:57 +0100

    Optimize RAM usage tracking and fix File Descriptor leak

--- main.cpp
+++ main.cpp
@@ -1,35 +1,52 @@
-#include <ctype.h>
+#include <cctype>
 #include <dirent.h>
 #include <fstream>
 #include <iostream>
 
 using namespace std;
 
-void listProcesses() {
-  DIR *dir = opendir("/proc");
+void procList() {
+  DIR *dir = opendir("/proc/");
+
   if (dir == NULL) {
     perror("Could not open /proc");
     return;
   }
 
   struct dirent *entry;
+  string word;
+
   while ((entry = readdir(dir)) != NULL) {
     if (isdigit(entry->d_name[0])) {
       string pid = entry->d_name;
-      string commPath = "/proc/" + pid + "/comm";
+      string statusFilePath = "/proc/" + pid + "/status";
 
-      ifstream commFile(commPath);
-      string procName;
+      ifstream statusFile(statusFilePath);
+      if (!statusFile.is_open())
+        continue;
 
-      if (getline(commFile, procName)) {
-        cout << "PID: " << pid << " \t Name: " << procName << endl;
+      string VmRSS = "0";
+      string procName = "Unknown";
+
+      while (statusFile >> word) {
+        if (word == "Name:") {
+          statusFile >> procName;
+        } else if (word == "VmRSS:") {
+          statusFile >> VmRSS;
+          break;
+        }
       }
+
+      cout << "PID: " << pid << " \t Name: " << procName << " \t RAM: " << VmRSS
+           << " kB" << endl;
+
+      statusFile.close();
     }
   }
   closedir(dir);
 }
 
 int main() {
-  listProcesses();
+  procList();
   return 0;
 }
